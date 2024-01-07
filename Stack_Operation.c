36892c4f3205581908b8375b6b6b2638df00fffb
Syed-Farhan-bmsce <155888143+Syed-Farhan-bmsce@users.noreply.github.com>
2024-01-07 17:53:29 +0530

    Update Stack_Operation.c

--- Stack_Operation.c
+++ Stack_Operation.c
@@ -1,4 +1,4 @@
-// 1.Write a program to simulate the working of stack using an array with the following :
+// Write a program to simulate the working of stack using an array with the following :
 // a) Push b) Pop c) Display
 // The program should print appropriate messages for stack overflow, stack underflow.
 
